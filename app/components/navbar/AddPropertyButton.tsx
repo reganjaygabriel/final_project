3a4d1f5e4bcffd627520330b0cd0c54d14e46cc0
Bonpire1 <regangabriel0@gmail.com>
2025-11-12 09:39:36 +0800

    V1-Code for nav bar, Gabriel, Regan Jay M.

---
+++ app/components/navbar/AddPropertyButton.tsx
@@ -0,0 +1,8 @@
+const AddPropertyButton = () => {
+  return (
+    <div className="p-2 cursor-pointer text-sm font-semibold rounded-full hover:bg-gray-200">
+      AirBnb
+    </div>
+  );
+};
+export default AddPropertyButton;
