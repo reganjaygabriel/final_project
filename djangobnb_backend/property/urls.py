0f1e45f4f5de2b2007b19706c5f04f06ea189135
Exanimate <regangabriel0@gmail.com>
2025-12-12 11:18:36 +0800

    Video 6 - Make it possible to set property on favorite - Gabriel, Regan Jay M.

--- djangobnb_backend/property/urls.py
+++ djangobnb_backend/property/urls.py
@@ -9,4 +9,5 @@ urlpatterns = [
     path('<uuid:pk>/', api.properties_detail, name='api_property_detail'),
     path('<uuid:pk>/book/', api.book_property, name='api_book_property'),
     path('<uuid:pk>/reservations/', api.property_reservations, name='api_property_reservations'),
+    path('<uuid:pk>/toggle_favorite/', api.toggle_favorite, name='api_toggle_favorite'),
 ]
