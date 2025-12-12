e80cc14e42ba53506525dd198150863e1d5686a9
Gabriel, Regan Jay M. <regangabriel0@gmail.com>
2025-12-12 15:58:42 +0800

    Video 7 - Send/Recieve messages - Gabriel, Regan Jay M.

--- djangobnb_backend/chat/consumers.py
+++ djangobnb_backend/chat/consumers.py
@@ -28,6 +28,7 @@ class ChatConsumer(AsyncWebsocketConsumer):
             self.channel_name
         )
 
+   
     # Recieve message from web sockets
     async def receive(self, text_data):
         data = json.loads(text_data)
@@ -45,8 +46,6 @@ class ChatConsumer(AsyncWebsocketConsumer):
                 'name': name
             }
         )
-
-        await self.save_message(conversation_id, body, sent_to_id)
     
     # Sending messages
     async def chat_message(self, event):
@@ -57,9 +56,3 @@ class ChatConsumer(AsyncWebsocketConsumer):
             'body': body,
             'name': name
         }))
-
-    @sync_to_async
-    def save_message(self, conversation_id, body, sent_to_id):
-        user = self.scope['user']
-
-        ConversationMessage.objects.create(conversation_id=conversation_id, body=body, sent_to_id=sent_to_id, created_by=user)
